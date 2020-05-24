298e1240940e98597c7ad1b75c0a853ea9d30a9f
O Padeiro <daviiddamiianbaker@gmail.com>
2020-05-24 00:14:46 -0400

    creating & adding author model

--- routes/authors.js
+++ routes/authors.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router()
-
+const Author = require('../models/author')
 // AUTHORS ROUTES
 router.get('/', (req, res) => {
     res.render('authors/index')
 })
 
-// NEW AUTHOR ROUTEgit status
+// NEW AUTHOR ROUTE
 router.get('/new', (req, res) => {
-    res.render('authors/new')
+    res.render('authors/new', { author: new Author() })
 })
 
 // CREATE AUTHOR ROUTE
